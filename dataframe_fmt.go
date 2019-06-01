6b2b715ce7f7bce77edf11c67831aeb2be60f6ab
rocketlaunchr-cto <rocketlaunchr.cloud@gmail.com>
2019-06-01 19:16:53 +1000

    - fmt.Println will sumarize dataframe when it contains more than 6 row.s

--- dataframe_fmt.go
+++ dataframe_fmt.go
@@ -79,5 +79,57 @@ func (df *DataFrame) Table(r ...Range) string {
 
 // String will display dataframe
 func (df *DataFrame) String() string {
-	return df.Table()
+
+	if df.NRows() <= 6 {
+		return df.Table()
+	}
+
+	df.lock.RLock()
+	defer df.lock.RUnlock()
+
+	idx := []int{0, 1, 2, df.n - 3, df.n - 2, df.n - 1}
+
+	data := [][]string{}
+
+	headers := []string{""} // row header is blank
+	footers := []string{fmt.Sprintf("%dx%d", df.n, len(df.Series))}
+	for _, aSeries := range df.Series {
+		headers = append(headers, aSeries.Name())
+		footers = append(footers, aSeries.Type())
+	}
+
+	for j, row := range idx {
+
+		if j == 3 {
+			sVals := []string{"⋮"}
+
+			for range df.Series {
+				sVals = append(sVals, "⋮")
+			}
+
+			data = append(data, sVals)
+		}
+
+		sVals := []string{fmt.Sprintf("%d:", row)}
+
+		for _, aSeries := range df.Series {
+			sVals = append(sVals, aSeries.ValueString(row))
+		}
+
+		data = append(data, sVals)
+	}
+
+	var buf bytes.Buffer
+
+	table := tablewriter.NewWriter(&buf)
+	table.SetHeader(headers)
+	for _, v := range data {
+		table.Append(v)
+	}
+	table.SetFooter(footers)
+	table.SetAlignment(tablewriter.ALIGN_CENTER)
+
+	table.Render()
+
+	return buf.String()
 }
