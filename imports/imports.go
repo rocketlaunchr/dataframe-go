70c73d278cfe76465fc71bf113cf47e90c84e6f7
rocketlaunchr-cto <rocketlaunchr.cloud@gmail.com>
2019-06-02 13:27:32 +1000

    - update docs - clean up file

--- imports/imports.go
+++ imports/imports.go
@@ -8,10 +8,8 @@ import (
 	"errors"
 	"fmt"
 	"io"
-	"strconv"
 	"time"
 
-	"github.com/davecgh/go-spew/spew"
 	dataframe "github.com/rocketlaunchr/dataframe-go"
 )
 
@@ -135,6 +133,7 @@ type JSONLoadOptions struct {
 }
 
 // LoadFromJSON will load data from a json file.
+// The first row determines which fields will be imported for subsequent rows.
 // WARNING: The API may change in the future.
 func LoadFromJSON(r io.ReadSeeker, options ...JSONLoadOptions) (*dataframe.DataFrame, error) {
 
@@ -328,89 +327,9 @@ func LoadFromJSON(r io.ReadSeeker, options ...JSONLoadOptions) (*dataframe.DataF
 							}
 						}
 					} else {
-						switch typ.(type) {
-						case float64:
-							// Force v to float64
-							switch v := val.(type) {
-							case string:
-								f, err := strconv.ParseFloat(v, 64)
-								if err != nil {
-									return nil, fmt.Errorf("can't force string to float64. row: %d field: %s", row-1, name)
-								}
-								insertVals[name] = f
-							case json.Number:
-								f, err := v.Float64()
-								if err != nil {
-									return nil, fmt.Errorf("can't force number to float64. row: %d field: %s", row-1, name)
-								}
-								insertVals[name] = f
-							case bool:
-								if v == true {
-									insertVals[name] = 1.0
-								} else {
-									insertVals[name] = 0.0
-								}
-							}
-						case int64:
-							// Force v to int64
-							switch v := val.(type) {
-							case string:
-								i, err := strconv.ParseInt(v, 10, 64)
-								if err != nil {
-									return nil, fmt.Errorf("can't force string to int64. row: %d field: %s", row-1, name)
-								}
-								insertVals[name] = i
-							case json.Number:
-								i, err := v.Int64()
-								if err != nil {
-									return nil, fmt.Errorf("can't force number to int64. row: %d field: %s", row-1, name)
-								}
-								insertVals[name] = i
-							case bool:
-								if v == true {
-									insertVals[name] = int64(1)
-								} else {
-									insertVals[name] = int64(0)
-								}
-							}
-						case string:
-							// Force v to string
-							switch v := val.(type) {
-							case string:
-								insertVals[name] = v
-							case json.Number:
-								insertVals[name] = v.String()
-							case bool:
-								if v == true {
-									insertVals[name] = "true"
-								} else {
-									insertVals[name] = "false"
-								}
-							}
-						case time.Time:
-							// Force v to time
-							switch v := val.(type) {
-							case string:
-								t, err := time.Parse(time.RFC3339, v)
-								if err != nil {
-									return nil, fmt.Errorf("can't force string to time.Time (%s). row: %d field: %s", time.RFC3339, row-1, name)
-								}
-								insertVals[name] = t
-							case json.Number:
-								// Assume unix timestamp
-								sec, err := v.Int64()
-								if err != nil {
-									return nil, fmt.Errorf("can't force number to int64 (unix timestamp). row: %d field: %s", row-1, name)
-								}
-								insertVals[name] = time.Unix(sec, 0)
-							case nil:
-								// Do nothing
-							default:
-								return nil, fmt.Errorf("can't force %T to time.Time. row: %d field: %s", v, row-1, name)
-							}
-						default:
-							// Force v to generic
-							panic("TODO: Not implemented")
+						err := dictateForce(row, insertVals, name, typ, val)
+						if err != nil {
+							return nil, err
 						}
 					}
 				} else {
@@ -429,10 +348,7 @@ func LoadFromJSON(r io.ReadSeeker, options ...JSONLoadOptions) (*dataframe.DataF
 						}
 					}
 				}
-
-				fmt.Println(name, spew.Sdump(val))
 			}
-			fmt.Println("--------", spew.Sdump(insertVals))
 
 			if len(insertVals) > 0 {
 				if init == nil {
