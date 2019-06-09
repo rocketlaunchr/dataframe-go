9571a97fbc325bc8f1954f0662ce53790f129736
propersam <propersam2012@gmail.com>
2019-06-10 00:31:12 +0100

    Finish Export CSV function

--- exports/export_csv.go
+++ exports/export_csv.go
@@ -2,7 +2,6 @@ package exports
 
 import (
 	"encoding/csv"
-	"fmt"
 	"io"
 
 	dataframe "github.com/rocketlaunchr/dataframe-go"
@@ -10,42 +9,55 @@ import (
 
 // CSVExportOptions contains options for CSV
 type CSVExportOptions struct {
-	Comma   rune // Field delimiter (set to ',' by NewWriter)
-	UseCRLF bool // True to use \r\n as the line terminator
+	NullString string          //optional param to specify what nil values should be encoded as (i.e. NULL, \N, NaN, NA etc)
+	Range      dataframe.Range // Range of data subsets to write from dataframe
+	Separator  rune            // Field delimiter (set to ',' by NewWriter)
+	UseCRLF    bool            // True to use \r\n as the line terminator
 }
 
 // ExportToCSV exports data object to CSV
-func ExportToCSV(w io.Writer, df dataframe.Dataframe, r ...dataframe.Range) error {
-	header := []string{""}
+func ExportToCSV(w io.Writer, df *dataframe.DataFrame, options ...CSVExportOptions) error {
+	header := []string{}
 	records := [][]string{}
+
+	r := dataframe.Range{} // initial default range r
+	NullString := "NaN"    // Set default value for null strings
 
 	cw := csv.NewWriter(w)
 
-	// if len(options) > 0 {
-	// 	cw.Comma = options[0].Comma
-	// 	cw.UseCRLF = options[0].UseCRLF
-	// }
+	if len(options) > 0 {
+		cw.Comma = options[0].Separator
+		cw.UseCRLF = options[0].UseCRLF
 
-	if len(r) == 0 {
-		r = append(r, dataframe.Range{})
+		if options[0].Range != r {
+			r = options[0].Range
+		}
+
+		if options[0].NullString != NullString {
+			NullString = options[0].NullString
+		}
 	}
 
 	for _, aSeries := range df.Series {
-		header = append(header, aSeries.Name)
+		header = append(header, aSeries.Name())
 	}
 	records = append(records, header)
 
-	if df.n > 0 {
-		s, e, err := r[0].Limits(df.n)
+	if df.NRows() > 0 {
+		s, e, err := r.Limits(df.NRows())
 		if err != nil {
 			return err
 		}
 
 		for row := s; row <= e; row++ {
-			sVals := []string{fmt.Sprintf("%d:", row)}
+			sVals := []string{}
 
 			for _, aSeries := range df.Series {
-				sVals = append(sVals, aSeries.ValueString(row))
+				val := aSeries.ValueString(row)
+				// if strings.ToLower(val) == "nan" || strings.ToLower(val) == "na" || strings.ToLower(val) == "null" {
+				// 	val = string(NullString)
+				// }
+				sVals = append(sVals, val)
 			}
 
 			records = append(records, sVals)
@@ -64,4 +76,5 @@ func ExportToCSV(w io.Writer, df dataframe.Dataframe, r ...dataframe.Range) erro
 		return err
 	}
 
+	return nil
 }
