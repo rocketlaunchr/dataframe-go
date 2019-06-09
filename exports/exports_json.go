fef10dc067e41a011df622f74a5820800e2c5407
propersam <propersam2012@gmail.com>
2019-06-10 00:31:12 +0100

    Finish Export CSV function

--- exports/exports_json.go
+++ exports/exports_json.go
@@ -1,6 +1,7 @@
 package exports
 
 import (
+	"encoding/json"
 	"io"
 
 	dataframe "github.com/rocketlaunchr/dataframe-go"
@@ -8,12 +9,34 @@ import (
 
 // JSONExportOptions contains options for JSON
 type JSONExportOptions struct {
-	Comma   rune // Field delimiter (set to ',' by NewWriter)
-	UseCRLF bool // True to use \r\n as the line terminator
+	Range dataframe.Range
 }
 
 // ExportToJSON exports data object to JSON
-func ExportToJSON(w io.Writer, df dataframe.Dataframe, r ...dataframe.Range) error {
+func ExportToJSON(w io.Writer, df *dataframe.DataFrame, options ...JSONExportOptions) error {
+	var r dataframe.Range
+
+	enc := json.NewEncoder(w)
+
+	records := []map[string]interface{}{}
+
+	if len(options) > 0 {
+
+		if options[0].Range != r {
+			r = options[0].Range
+		}
+
+	}
+
+	// TODO: prepare df.Series into Name and value
+	// Struct interface
+	// to be encoded by enc.
+
+	for _, record := range records {
+		if err := enc.Encode(record); err != nil {
+			return err
+		}
+	}
 
 	return nil
 }
