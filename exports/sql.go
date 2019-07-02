17ae62a25ea919b2bf83566d3a45440fab4d36bd
rocketlaunchr-cto <rocketlaunchr.cloud@gmail.com>
2019-07-02 11:22:05 +1000

    - an an example

--- exports/sql.go
+++ exports/sql.go
@@ -66,6 +66,56 @@ type PrimaryKey struct {
 // ExportToSQL exports a dataframe to a SQL Database.
 // It is assumed to be a PostgreSQL database (for placeholder purposes), unless
 // otherwise set to MySQL using the Options.
+//
+// Example:
+//
+// import (
+// 	stdSql "database/sql"
+//
+// 	"github.com/rocketlaunchr/dataframe-go/exports"
+// 	sql "github.com/rocketlaunchr/mysql-go"
+// )
+
+// func main() {
+//
+// 	p, _ := stdSql.Open("mysql", "user:password@/dbname")
+// 	kP, _ := stdSql.Open("mysql", "user:password@/dbname")
+// 	kP.SetMaxOpenConns(1)
+//
+// 	pool := &sql.DB{p, kP}
+//
+// 	conn, err := pool.Conn(ctx)
+// 	defer conn.Close()
+
+// 	tx, _ := conn.BeginTx(ctx, nil)
+//
+// 	opts := exports.SQLExportOptions{
+// 		SeriesToColumn: map[string]*string{
+// 			"Country": &[]string{"country"}[0],
+// 			"Age":     &[]string{"age"}[0],
+// 			"Id":      nil,
+// 			"Date":    nil,
+// 			"Amount":  nil,
+// 		},
+// 		PrimaryKey: &exports.PrimaryKey{
+// 			PrimaryKey: "uuid",
+// 			Value: func(row int, n int) *string {
+// 				str := uuid.NewV4().String()
+// 				return &str
+// 			},
+// 		},
+// 		BatchSize: &[]uint{50}[0],
+// 		Database:  exports.MySQL,
+// 	}
+//
+// 	err = exports.ExportToSQL(ctx, tx, df, "test", opts)
+// 	if err != nil {
+// 		return tx.Rollback()
+// 	}
+//
+// 	tx.Commit()
+// }
+//
 func ExportToSQL(ctx context.Context, db execContexter, df *dataframe.DataFrame, tableName string, options ...SQLExportOptions) error {
 
 	df.Lock()
