0452b0f6ae71a2e94917c4f5731de2477b158651
rocketlaunchr-cto <rocketlaunchr.cloud@gmail.com>
2020-02-24 11:04:57 +1100

    - update interface

--- forecast/interface.go
+++ forecast/interface.go
@@ -1,6 +1,8 @@
 package forecast
 
 import (
+	"context"
+
 	dataframe "github.com/rocketlaunchr/dataframe-go"
 )
 
@@ -9,10 +11,10 @@ type Algorithm interface {
 	// Load loads historical data. sdf can be a SeriesFloat64 or DataFrame.
 	Load(sdf interface{}, r *dataframe.Range)
 
-	// Forecast forecasts the next n values for a Series or DataFrame.
+	// Predict forecasts the next n values for a Series or DataFrame.
 	// If a Series was provided to Load function, then a Series is retured.
 	// Alternatively a DataFrame is returend.
-	Forecast(n int) interface{}
+	Predict(ctx context.Context, n int) (interface{}, error)
 
 	// Configure sets the various parameters for the Algorithm.
 	// config must be a struct that the particular Algorithm understands.
@@ -21,7 +23,7 @@ type Algorithm interface {
 	// Validate can be used by providing a validation set of data.
 	// It will then forecast the values from the end of the loaded data and then compare
 	// them with the validation set.
-	Validate(sdf interface{}, r *dataframe.Range, errorType ErrorType) float64
+	Validate(ctx context.Context, sdf interface{}, r *dataframe.Range, errorType ErrorType) (float64, error)
 }
 
 // ExponentialSmootheningConfig is used to configure the ETS algorithm.
