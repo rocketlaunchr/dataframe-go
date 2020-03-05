d810c8f4774d4ea85e476e4eb565d352dd728858
Samuel Ameh <propersam2012@gmail.com>
2020-03-05 01:50:02 +0100

    remove unwanted code from holtWinters

--- forecast/holt_winters.go
+++ forecast/holt_winters.go
@@ -40,7 +40,6 @@ func NewHwModel() *HwModel {
 		data:                 []float64{},
 		trainData:            []float64{},
 		testData:             []float64{},
-		fcastData:            &dataframe.SeriesFloat64{},
 		initialSmooth:        0.0,
 		initialTrend:         0.0,
 		initialSeasonalComps: []float64{},
@@ -51,7 +50,6 @@ func NewHwModel() *HwModel {
 		alpha:                0.0,
 		beta:                 0.0,
 		gamma:                0.0,
-		errorM:               &ErrorMeasurement{},
 		inputIsDf:            false,
 	}
 
@@ -268,178 +266,6 @@ func (hm *HwModel) trainModel(ctx context.Context, start, end int) error {
 	hm.seasonalComps = seasonals
 
 	return nil
-}
-
-// Fit Method performs the splitting and training of the HwModel based on the Tripple Exponential Smoothing algorithm.
-// It returns a trained HwModel ready to carry out future predictions.
-// The arguments α, beta nd gamma must be between [0,1]. Recent values receive more weight when α is closer to 1.
-func (hm *HwModel) Fit(ctx context.Context, tr *dataframe.Range, opts interface{}, et ...ErrorType) (*HwModel, error) {
-
-	var (
-		α, β, γ        float64
-		period         int
-		r              *dataframe.Range
-		errTyp         ErrorType
-		trnd, prevTrnd float64 // trend
-	)
-
-	if o, ok := opts.(HoltWintersConfig); ok {
-
-		α = o.Alpha
-		β = o.Beta
-		γ = o.Gamma
-		period = o.Period
-
-	} else {
-		return nil, errors.New("fit options passed is not compartible with holtwinters model")
-	}
-
-	r = &dataframe.Range{}
-	if tr != nil {
-		r = tr
-	}
-
-	if len(et) > 0 {
-		errTyp = et[0]
-	}
-
-	start, end, err := r.Limits(len(hm.data))
-	if err != nil {
-		return nil, err
-	}
-
-	// Validation
-	if end-start < 1 {
-		return nil, errors.New("no values in series range")
-	}
-
-	if (α < 0.0) || (α > 1.0) {
-		return nil, errors.New("α must be between [0,1]")
-	}
-
-	if (β < 0.0) || (β > 1.0) {
-		return nil, errors.New("β must be between [0,1]")
-	}
-
-	if (γ < 0.0) || (γ > 1.0) {
-		return nil, errors.New("γ must be between [0,1]")
-	}
-
-	trainData := hm.data[start : end+1]
-	hm.trainData = trainData
-
-	testData := hm.data[end+1:]
-	if len(testData) < 3 {
-		return nil, errors.New("There should be a minimum of 3 data left as testing data")
-	}
-	hm.testData = testData
-
-	testSeries := dataframe.NewSeriesFloat64("Test Data", nil, testData)
-
-	hm.alpha = α
-	hm.beta = β
-	hm.gamma = γ
-
-	y := hm.data[start : end+1]
-
-	seasonals := initialSeasonalComponents(y, period)
-
-	hm.initialSeasonalComps = initialSeasonalComponents(y, period)
-
-	trnd = initialTrend(y, period)
-	hm.initialTrend = trnd
-
-	var st, prevSt float64 // smooth
-
-	for i := start; i < end+1; i++ {
-		// Breaking out on context failure
-		if err := ctx.Err(); err != nil {
-			return nil, err
-		}
-
-		xt := y[i]
-
-		if i == start { // Set initial smooth
-			st = xt
-
-			hm.initialSmooth = xt
-
-		} else {
-			// multiplicative method
-			// prevSt, st = st, α * (xt / seasonals[i % period]) + (1 - α) * (st + trnd)
-			// prevTrnd, trnd = trnd, β * (st - prevSt) + (1 - β) * trnd
-			// seasonals[i % period] = γ * (xt / (prevSt + prevTrnd)) + (1 - γ) * seasonals[i % period]
-
-			// additive method
-			prevSt, st = st, α*(xt-seasonals[i%period])+(1-α)*(st+trnd)
-			prevTrnd, trnd = trnd, β*(st-prevSt)+(1-β)*trnd
-			seasonals[i%period] = γ*(xt-prevSt-prevTrnd) + (1-γ)*seasonals[i%period]
-			// _ = prevTrnd
-			// fmt.Println(st + trnd + seasonals[i % period])
-		}
-
-	}
-
-	// This is for the test forecast
-	fcast := []float64{}
-	m := 1
-	for k := end + 1; k < len(hm.data); k++ {
-		if err := ctx.Err(); err != nil {
-			return nil, err
-		}
-		// multiplicative Method
-		// fcast = append(fcast, (st + float64(m)*trnd) * seasonals[(m-1) % period])
-
-		// additive method
-		fcast = append(fcast, (st+float64(m)*trnd)+seasonals[(m-1)%period])
-
-		m++
-	}
-
-	fcastSeries := dataframe.NewSeriesFloat64("Forecast Data", nil)
-	fcastSeries.Values = fcast
-	hm.fcastData = fcastSeries
-
-	hm.smoothingLevel = st
-	hm.trendLevel = trnd
-	hm.period = period
-	hm.seasonalComps = seasonals
-
-	// NOw to calculate the Errors
-	errOpts := &ErrorOptions{}
-
-	var val float64
-
-	if errTyp == MAE {
-		val, _, err = MeanAbsoluteError(ctx, testSeries, fcastSeries, errOpts)
-		if err != nil {
-			return nil, err
-		}
-	} else if errTyp == SSE {
-		val, _, err = SumOfSquaredErrors(ctx, testSeries, fcastSeries, errOpts)
-		if err != nil {
-			return nil, err
-		}
-	} else if errTyp == RMSE {
-		val, _, err = RootMeanSquaredError(ctx, testSeries, fcastSeries, errOpts)
-		if err != nil {
-			return nil, err
-		}
-	} else if errTyp == MAPE {
-		val, _, err = MeanAbsolutePercentageError(ctx, testSeries, fcastSeries, errOpts)
-		if err != nil {
-			return nil, err
-		}
-	} else {
-		return nil, errors.New("Unknown error type")
-	}
-
-	hm.errorM = &ErrorMeasurement{
-		errorType: errTyp,
-		value:     val,
-	}
-
-	return hm, nil
 }
 
 // Validate can be used by providing a validation set of data.
